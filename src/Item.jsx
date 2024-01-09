60dad07b13caaf8dbe7cbc4f7d7a40d5e29a59cb
Galaxeo <hwjustincheok@gmail.com>
2024-01-08 22:20:49 -0800

    fix: item formatting, item grid

--- src/Item.jsx
+++ src/Item.jsx
@@ -4,7 +4,7 @@ import "./App.css";
 function Item({ image, name, price }) {
   return (
     <div className="item">
-      <img src={image} alt={name} />
+      <img className="itemImg" src={image} alt={name} />
       <h2>{name}</h2>
     </div>
   );
